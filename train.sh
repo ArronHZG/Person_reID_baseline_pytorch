cdfe2411a22d62a8838f5512ce16a438da468f03
ArronHZG <hou.zg@foxmail.com>
2019-11-12 15:02:57 +0800

    save and valid

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
-#python train.py --gpu_ids 0 --name ft_ResNet50 --train_all --batchsize 32  --data_dir /home/arron/dataset/Market/pytorch
+#python train.py --gpu_ids 0 --name ft_ResNet50 --batchsize 32  --data_dir /home/arron/dataset/Market/pytorch
 
 # ft_ResNet50
 # torch.Size([3368, 512])
@@ -7,10 +7,12 @@
 
 python train.py \
 --gpu_ids 0 \
---name \
-ft_ResNet50 \
---train_all \
+--name ft_ResNet50_PCB \
 --batchsize 32  \
 --data_dir \
 /home/arron/dataset/Market/pytorch \
 --PCB
+
+#ft_ResNet50
+#torch.Size([3368, 12288])
+#Rank@1:0.923990 Rank@5:0.971793 Rank@10:0.981591 mAP:0.776559
